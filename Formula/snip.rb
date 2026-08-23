f9bc4ee91365f1926dc23cc83e3324fa50b6115c
cian.z <hi_leo@yeah.net>
2026-08-23 17:44:16 +0800

    Formula/snip.rb: 填入真实的 SHA256 校验值

--- Formula/snip.rb
+++ Formula/snip.rb
@@ -4,7 +4,7 @@ class Snip < Formula
   desc "macOS clipboard and keyboard diagnostic tool"
   homepage "https://github.com/clanzhang/snip"
   url "https://github.com/clanzhang/snip/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_SHA256"
+  sha256 "2bd858b0d3979891f50d2a9c106a190b21af8aa1a1cf2405a498e77c86d751ac"
   license "MIT"
 
   depends_on :macos
