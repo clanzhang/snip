c57af63b7590c451bb360c4c922cfd565795731d
cian.z <hi_leo@yeah.net>
2026-08-23 17:26:54 +0800

    Formula/snip.rb: 更新为 Homebrew 官方规范，支持 brew install clanzhang/tap/snip

---
+++ Formula/snip.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Snip < Formula
+  desc "macOS clipboard and keyboard diagnostic tool"
+  homepage "https://github.com/clanzhang/snip"
+  url "https://github.com/clanzhang/snip/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "REPLACE_WITH_SHA256"
+  license "MIT"
+
+  depends_on :macos
+  depends_on xcode: ["14.0", :build]
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/snip"
+  end
+
+  test do
+    system "#{bin}/snip", "--version"
+  end
+end
